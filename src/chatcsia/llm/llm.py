da1a545a3f3d250dc6281dca48de3d7d17fc262b
valentinbiolley <valentin.biolley@hefr.ch>
2026-06-02 16:11:16 +0200

    fix : Add placeholder OpenAI API key to model kwargs

--- src/chatcsia/llm/llm.py
+++ src/chatcsia/llm/llm.py
@@ -35,6 +35,7 @@ def prepare_model(
     model_kwargs = {
         "model": model_name,
         "callback_manager": callback_manager,
+        "openai_api_key" : "fake_keys"
         "temperature": temperature,
         "stop": ["[/INST]", "</s>", "<|im_end|>", "<</SYS>>"],
     }
